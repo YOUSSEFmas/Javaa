f7e72fa1d653d00f47aa07310c4bed6b02586174
Hayani soujaa youssef <hayani@gmail.com>
2017-09-30 00:14:19 +0200

    .gitignore CC

--- src/SimpleFraction/Main.java
+++ src/SimpleFraction/Main.java
@@ -6,6 +6,7 @@ public class Main {
 		System.out.println(f.toString());
 
 		System.out.println(f.toString());
+		System.out.println(f.toString());System.out.println(f.toString());System.out.println(f.toString());System.out.println(f.toString());
 	}
 
 }
