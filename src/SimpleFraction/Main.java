010532ba54efc33944b8509f114c909e0dbf51e7
Hayani soujaa youssef <hayani@gmail.com>
2017-09-30 00:27:38 +0200

    lll

--- src/SimpleFraction/Main.java
+++ src/SimpleFraction/Main.java
@@ -4,9 +4,11 @@ public class Main {
 	public static void main(String[] args) {
 		Fraction f= new Fraction(5, 1);
 		System.out.println(f.toString());
-
+		
+		
+		
 		System.out.println(f.toString());
-		System.out.println(f.toString());System.out.println(f.toString());System.out.println(f.toString());System.out.println(f.toString());
+		System.out.println(f.toString());
 	}
 
 }
