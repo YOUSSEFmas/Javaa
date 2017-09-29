1dbd6142ee1327cff76972a16a0c7c842dee3e97
Hayani soujaa youssef <hayani@gmail.com>
2017-09-30 00:31:38 +0200

    lllxxx

--- src/SimpleFraction/Main.java
+++ src/SimpleFraction/Main.java
@@ -2,13 +2,11 @@ package SimpleFraction;
 
 public class Main {
 	public static void main(String[] args) {
-		Fraction f= new Fraction(5, 1);
+		Fraction f= new Fraction(3, 1);
 		System.out.println(f.toString());
 		
 		
 		
-		System.out.println(f.toString());
-		System.out.println(f.toString());
 	}
 
 }
